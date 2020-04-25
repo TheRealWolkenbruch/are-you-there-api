50a841e2837ac4ba263d98a4288e2658a2acc1f7
Raul Andres <raul.andresrodriguez@telefonica.com>
2020-04-25 17:10:17 +0200

    Integrate Roda and Sequel

---
+++ tests/auth.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+
+
+TIMESTAMP=`date +%s`
+HEADER_FILE=/tmp/header-${TIMESTAMP}.log
+
+
+try_failed_auth () {
+  curl \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: foobar" \
+    localhost:9292/api/wards
+
+}
+
+try_out_login () {
+  curl \
+    -H 'Content-Type: application/json' \
+    --data '{"login": "JohnDoe", "password": "JohnDoe1"}' \
+    --dump-header "$HEADER_FILE" \
+    localhost:9292/login
+
+  JWT_TOKEN=$(grep '^Authorization' "$HEADER_FILE" | cut -d: -f2 | tr -d '[:space:]')
+
+  curl \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: ${JWT_TOKEN}" \
+    localhost:9292/api/wards
+}
+
+main () {
+  echo '#################'
+  echo 'Try without login'
+  echo '#################'
+  echo ''
+  try_failed_auth
+  echo ''
+
+  echo ''
+  echo '#################'
+  echo 'Try without login'
+  echo '#################'
+  echo ''
+  try_out_login
+}
+
+main
