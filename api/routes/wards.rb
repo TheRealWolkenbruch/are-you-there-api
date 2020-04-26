d5152c069d08a30db04239fb1130e3abaeb862f5
Raul Andres <raul.andresrodriguez@telefonica.com>
2020-04-26 10:03:52 +0200

    Use multi_route plugin

--- api/routes/wards.rb
+++ api/routes/wards.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class App < Roda
-  def wards(r)
+  route('wards') do |r|
     r.get do
       account_id = rodauth.jwt_session_hash[:account_id]
       DB[:wards].where(f_guardian_id: account_id).map { |v| v.reject { |k, _v| k == :password } }.to_json
